0db3c6e81619a6a38531bf2024ffc3cc813953a6
Alex Vallejo <alxvallejo@gmail.com>
2020-04-02 19:17:22 -0400

    Add firebase config

---
+++ src/client/services/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: 'AIzaSyDWubr0-INUleZRUkL492BJEHSNi-WT66E',
+	authDomain: 'areta-9cc95.firebaseapp.com',
+	databaseURL: 'https://areta-9cc95.firebaseio.com',
+	projectId: 'areta-9cc95',
+	storageBucket: 'areta-9cc95.appspot.com',
+	messagingSenderId: '483191412342',
+	appId: '1:483191412342:web:add3d2e8c2f78b1c29f157'
+};
